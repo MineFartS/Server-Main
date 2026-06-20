87276d51813f8a3f466e01da186343ee31b0dbf9
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-20 08:02:40 -0400

    Update Create.ps1

--- Virtual Machines/Create.ps1
+++ Virtual Machines/Create.ps1
@@ -4,23 +4,20 @@ param(
 )
 
 Import-Module 'E:/Virtual Machines/mod.psm1' -Function Repair-VirtualMachine
-
-# Declare VM dir
-$Root = "E:\Virtual Machines"
-
 #=====================================================================================================
 
-# Create Dir
+$Dir = "E:\Virtual Machines\Hyper-V\$Name"
+
 New-Item `
-    -Path "$Root\Hyper-V\$Name" `
+    -Path $Dir `
     -ItemType Directory `
     -ErrorAction SilentlyContinue `
     -Verbose
 
 New-VHD `
-    -Path "$Root\Hyper-V\$Name\Hard Drive.vhdx"
-    -ParentPath "$Root\Template\Tiny11\Hard Drive.vhdx" `
-    -Differencing
+    -Path "$Dir\Hard Drive.vhdx" `
+    -SizeBytes 150GB `
+    -Dynamic
 
 Repair-VirtualMachine -Name $Name
 
