6d8d799238b43498fa4095982a9b126a438b848b
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-20 22:16:26 -0400

    Force Set Package

--- Minecraft/Running.py
+++ Minecraft/Running.py
@@ -1,10 +1,16 @@
+from philh_myftp_biz.terminal import cls, set_package
+
+set_package('E:/Minecraft')
+
 from .World import Worlds
 from . import Tasks
 
 for w in Worlds:
     if Tasks[w.name].running:
+        cls()
         print('true')
         break
 
 else:    
+    cls()
     print('false')
