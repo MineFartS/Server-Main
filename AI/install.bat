83500c8f0898d3796bd66f9e68fb85d66a2809c2
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-29 05:29:26 -0400

    Update AI venv to python 3.12.10

--- AI/install.bat
+++ AI/install.bat
@@ -1,8 +1,9 @@
-@echo off
+
+git submodule update --init --remote AI/.venv
 
 set py=%~dp0.venv\python.exe
 
-%py% -m pip install hatchling hatch-requirements-txt
+%py% -m pip install --upgrade pip setuptools wheel hatchling hatch-requirements-txt
 
 %py% -m pip install git+https://github.com/MineFartS/Server-PythonPackage --ignore-requires-python --no-build-isolation
 
