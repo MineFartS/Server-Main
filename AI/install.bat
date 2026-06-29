67b0e109859b44ceb9a66a139d110917b26cd91b
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-29 05:45:12 -0400

    Fix missing diffusers

--- AI/install.bat
+++ AI/install.bat
@@ -3,11 +3,11 @@ git submodule update --init --remote AI/.venv
 
 set py=%~dp0.venv\python.exe
 
-%py% -m pip install --upgrade pip setuptools wheel hatchling hatch-requirements-txt
+%py% -m pip install -U pip setuptools wheel hatchling hatch-requirements-txt
 
 %py% -m pip install git+https://github.com/MineFartS/Server-PythonPackage --ignore-requires-python --no-build-isolation
 
-%py% -m pip install ollama diffusers transformers accelerate
+%py% -m pip install -U ollama diffusers transformers accelerate
 
-%py% -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+%py% -m pip install -U torch torchvision --index-url https://download.pytorch.org/whl/cu126
 
