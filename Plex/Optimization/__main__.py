78d588e5a044b9f467d7bfccaf04ca371c84d8a6
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-11 19:25:33 -0400

    Only inspect video ext

---
+++ Plex/Optimization/__main__.py
@@ -0,0 +1,42 @@
+from ffmpeg_progress_yield import FfmpegProgress
+from philh_myftp_biz.programs import FFMPEG
+from philh_myftp_biz.terminal import Log
+from philh_myftp_biz.file import temp
+from philh_myftp_biz.pc import Path
+from tqdm import tqdm
+
+pbar = tqdm(
+    total = 100,
+    position = 1,
+    desc = "Encoding"
+)
+
+for src in Path('E:/Plex/Media/').descendants:
+
+    if (src.type != 'video') or (src.ext == 'mp4'): continue
+
+    dst = src.with_ext('mp4')
+    tmp = temp('encoding', 'mp4')
+
+    pbar.reset()
+
+    try:
+
+        ff = FfmpegProgress([
+            FFMPEG().path, # Ffmpeg.exe
+            '-hwaccel', 'cuda', # Use GPU
+            '-i', src.path, # Input Path
+            '-c:v', 'h265_nvenc', # Video Codec
+            '-c:a', 'aac', # Audio Codec
+            tmp.path, # Output Path
+        ])
+
+        for progress in ff.run_command_with_progress():
+            pbar.update(progress - pbar.n)
+        
+        tmp.move(dst)
+        src.delete()
+
+    except RuntimeError:
+        Log.FAIL(exc_info=True)
+
