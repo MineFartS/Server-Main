2c691b829dd7e0a64e0825b844032c528e2e0763
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-22 19:19:06 -0400

    Update __main__.py

--- Website/Indexer/__main__.py
+++ Website/Indexer/__main__.py
@@ -9,31 +9,30 @@ from re import sub
 # Clear the search registry
 Search.save([])
 
+dirs = [root] + [p for p in root.descendants if p.is_dir]
+
 # Iter through all descendants of root
-for p in root.descendants:
-    
-    # If the path is a directory 
-    if p.is_dir:
+for p in dirs:
 
-        registry = IndexRegistry(p)
+    registry = IndexRegistry(p)
 
-        Log.INFO(f'Building Registry: {registry}')
+    Log.INFO(f'Building Registry: {registry}')
 
-        # Clear the directory registry
-        registry.save([])
+    # Clear the directory registry
+    registry.save([])
 
-        # Iter through all items in the registry
-        for child in registry.children:
+    # Iter through all items in the registry
+    for child in registry.children:
 
-            entry = IndexEntry(child)
-            
-            Log.VERB(f'Adding Entry: {entry}')
+        entry = IndexEntry(child)
+        
+        Log.VERB(f'Adding Entry: {entry}')
 
-            # Append the entry to the directory registry
-            registry += entry.JSON
+        # Append the entry to the directory registry
+        registry += entry.JSON
 
-            # Append the entry to the search registry
-            Search += entry.JSON
+        # Append the entry to the search registry
+        Search += entry.JSON
 
 # ================================================================================================================
 # Update web.config
