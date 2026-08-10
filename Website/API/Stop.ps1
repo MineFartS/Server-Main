194f52abb319f0f865dfb58641a92f4011d069ca
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-10 10:39:40 -0400

    Update Stop.ps1

--- Website/API/Stop.ps1
+++ Website/API/Stop.ps1
@@ -2,3 +2,8 @@
 Get-Content -Path "$PSScriptRoot\__pycache__\PID.json" -Raw | ConvertFrom-Json | ForEach-Object {
     Stop-Process -Id $_ -Force
 }
+
+Stop-Process `
+    -Id (Get-NetTCPConnection -LocalPort 8000).OwningProcess `
+    -Force
+
