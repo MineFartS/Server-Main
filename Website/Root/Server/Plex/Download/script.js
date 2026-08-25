704121f287b8cd1115bf66f64317e15b066da7ed
Phil Hunt <philiphuntiv2@gmail.com>
2026-08-25 09:42:52 -0400

    Impl search.js in Plex Downloader

--- Website/Root/Server/Plex/Download/script.js
+++ Website/Root/Server/Plex/Download/script.js
@@ -1,10 +1,40 @@
 
+postSearch(async (term) => {
+
+    let results = []
+
+    let prom = await fetch(`https://www.omdbapi.com/?apikey=97f79170&s=${term}`);
+    let resp = await prom.json();
+
+    if (resp.Response == 'True') {
+
+        for (let i of resp.Search) {
+
+            // Parse the media release year
+            i.Year = i.Year.substring(0,4)
+
+            results.push(new ImageResult(
+                img = i.Poster,
+                callback = () => getItem(i.Type, i.Title, i.Year),
+                hover = `${i.Title} (${i.Year})`,
+                ig_err = true
+            ))
+            
+        }
+
+    }
+
+    return results
+
+})
+
+
 function getItem(Type, Title, Year) {
 
     // Save the current html of the search results
-    window.oldHTML = e.options.innerHTML
+   window.oldNodes = Array.from(e.options.childNodes);
 
-    e.search.setAttribute('readonly', 'true')
+    e.Search.setAttribute('readonly', 'true')
 
     // Add loading dots
 	e.options.innerHTML = `
@@ -49,11 +79,19 @@ function responseHandler(t) {
     // Show an alert with the response message
     alert(t)
 
+    // Clear the loading Dots
+    e.options.innerHTML = ''
+    
     // Restore the saved search results html
-    e.options.innerHTML = oldHTML
+    if (window.oldNodes) {
+        for (let node of window.oldNodes) {
+            e.options.appendChild(node);
+        }
+    }
 
     // Allow the search box to be modified
     e.search.removeAttribute('readonly')
 
 }
+
 
