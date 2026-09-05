808a4c2a3ca8f8344f0f5c217f14e7c11bb0cd28
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-05 09:52:36 -0400

    Link "C:\Windows\System32\inetsrv\config"

--- Users/FTP/Start.ps1
+++ Users/FTP/Start.ps1
@@ -2,3 +2,17 @@
 Enable-NetFirewallRule -DisplayGroup "FTP Server"
 
 Start-IISSite -Name 'FTP Server'
+
+$Src = "C:\Windows\System32\inetsrv\config\"
+$Dst = "E:\Users\FTP\IIS\"
+
+if ((Get-Item $Dst).Attributes -notmatch "ReparsePoint") {
+    Remove-Item $Dst -Recurse -Force
+}
+
+New-Item `
+    -Path $Dst `
+    -Value $Src `
+    -ItemType Junction `
+    -Force
+
